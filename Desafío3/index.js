10b9f6ed528509bb5185dabd5be1a3a8b561d414
annie <fav.hala@gmail.com>
2023-02-07 11:59:08 -0300

    Desafio3

--- Desafío3/index.js
+++ Desafío3/index.js
@@ -1,49 +1,30 @@
 
-// import express from 'express'
-// import ProductManager, { Product }  from './ProductManeger.js';
+import express from 'express'
+import ProductManager, { Product }  from './ProductManeger.js';
 
-// const myProductManager = new ProductManager('/Users/annie/Desktop/Backend/Desafío3/products.json')
+const myProductManager = new ProductManager('/Users/annie/Desktop/Backend/Desafío3/products.json')
 
-// const app = express();
-// const PORT = 4000;
+const app = express();
+const PORT = 4000;
 
-// app.get('/products', (req, res) => {
-//     const list_product = '/Users/annie/Desktop/Backend/Desafío3/products.json';
-//     res.send(JSON.parse(list_product))
-// })
+app.get('/products', async (req, res) => {
+    const list_product = await myProductManager.getProducts();
+    let {limite} = req.query
+    let data;
+    if (!limite) {
+      data = list_product;
+    } else {
+      data = list_product.slice(0, parseInt(limite))
+    }
+    res.send(data)
+})
 
-// app.get('/products:limite', (req, res) => {
-//     let {limite} = req.query
-//     const arrayProducts = JSONparse('/Users/annie/Desktop/Backend/Desafío3/products.json')
-//     if (limite) {
-//       const productCopia = arrayProducts.slice(0, limite)
-//     } else {
-//       res.send(JSON.stringify(arrayProducts))
-//     }
-// })
-// app.get('/user/:idUser', async (req, res) => {
-//     // console.log(req.params.idUser)
-//     const idUser = req.params.idUser
-//     const user = users.find(user => user.id === parseInt(idUser))
-//     if(user) {
-//         res.send(`Nombre de usuario ${user.nombre}`)
-//     } else {
-//         res.send("El usuario no existe")
-//     }
-// })
+app.get('/products/:id', async (req, res) => {
+   const product = await myProductManager.getProductById(parseInt(req.params.id));
+   product === null ? res.send("No se encontro el producto") : res.send(product)
+   
+})
 
-// app.get('/createProduct', async (req, res) => {
-//     const product1 = new Product("T-shirt Woman", "Excellent T-shirt", 5, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUz2udYJSW8es9xZztGlbA6tO9xjviF-tSmv9BxfET0Bf9b_nKAulzEWEBFomNJC-5u6k&usqp=CAU", 1, 10)
-//     await instanceProductManager.addProduct(product1);
-//     res.json(product1)
-// })
-
-// app.get('/deleteProduct', async (req, res) => {
-//     const id = req.params.id
-//     await instanceProductManager.deleteProduct(id);
-//     res.send(`product with ${id}, deleted`)
-// })
-
-// app.listen(PORT, () => {
-//     console.log(`Server on port ${PORT}`)
-// })
+app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`)
+})
