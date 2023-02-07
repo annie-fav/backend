0814ec8fbf75647333114f77bdb701ede1098606
annie <fav.hala@gmail.com>
2023-02-07 20:58:27 -0300

    Clase 9

---
+++ Clase 8 y 9/src/index.js
@@ -0,0 +1,59 @@
+import express from 'express'
+import { __dirname, __filename } from "./path.js"
+import productRouter from "/Users/annie/Desktop/Backend/Clase 8 y 9/src/routes/products.routes.js"
+import multer from 'multer'
+
+// const upload = multer({ dest:'src/public/img'}) . // ---> forma basica de cargar multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'src/public/img')
+    },
+
+    filename: (req, file, cb) => {
+        cb(null, `${file.originalname}`)
+    }
+})
+
+const upload = multer({storage: storage})
+
+const app = express() //app es igual a la ejecucion de express
+const PORT = 4000
+
+//Middlewares
+app.use(express.urlencoded({extended:true})) // Permite realizar consultas en la url (req.query)
+app.use(express.json()); // Permite trabajar con json
+
+// Routes
+
+// app.get('/info', (req, res) => {
+//     return 'Its working'
+// })
+
+// app.post('/more-info', (req, res) => {
+//     return 'Its more info'
+// })
+
+app.use('./static', express.static(__dirname + '/public')) // Defino mis archivos estaticos
+app.use('/api/products', productRouter)
+
+// Multer
+app.post('/upload', upload.single('product'), (req, res) => {
+    console.log(req.body)
+    console.log(req.file)
+    res.send("Imagen cargada")
+})
+
+//
+app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`)
+})
+
+
+
+
+
+
+
+
+
+
--- Desafío3/index.js
+++ Desafío3/index.js
@@ -1,30 +1,30 @@
 
-import express from 'express'
-import ProductManager, { Product }  from './ProductManeger.js';
+// import express from 'express'
+// import ProductManager, { Product }  from './ProductManeger.js';
 
-const myProductManager = new ProductManager('/Users/annie/Desktop/Backend/Desafío3/products.json')
+// const myProductManager = new ProductManager('/Users/annie/Desktop/Backend/Desafío3/products.json')
 
-const app = express();
-const PORT = 4000;
+// const app = express();
+// const PORT = 4000;
 
-app.get('/products', async (req, res) => {
-    const list_product = await myProductManager.getProducts();
-    let {limite} = req.query
-    let data;
-    if (!limite) {
-      data = list_product;
-    } else {
-      data = list_product.slice(0, parseInt(limite))
-    }
-    res.send(data)
-})
+// app.get('/products', async (req, res) => {
+//     const list_product = await myProductManager.getProducts();
+//     let {limite} = req.query
+//     let data;
+//     if (!limite) {
+//       data = list_product;
+//     } else {
+//       data = list_product.slice(0, parseInt(limite))
+//     }
+//     res.send(data)
+// })
 
-app.get('/products/:id', async (req, res) => {
-   const product = await myProductManager.getProductById(parseInt(req.params.id));
-   product === null ? res.send("No se encontro el producto") : res.send(product)
+// app.get('/products/:id', async (req, res) => {
+//    const product = await myProductManager.getProductById(parseInt(req.params.id));
+//    product === null ? res.send("No se encontro el producto") : res.send(product)
    
-})
+// })
 
-app.listen(PORT, () => {
-    console.log(`Server on port ${PORT}`)
-})
+// app.listen(PORT, () => {
+//     console.log(`Server on port ${PORT}`)
+// })
