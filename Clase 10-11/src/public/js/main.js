37dbb7bbb2c9f3e9888aeaf0692577ba621f71b2
annie <fav.hala@gmail.com>
2023-02-16 17:57:12 -0300

    clase-11

--- Clase 10-11/src/public/js/main.js
+++ Clase 10-11/src/public/js/main.js
@@ -1,5 +1,10 @@
 
 const socket = io()
+const form = document.getElementeById(idForm)
+form.addEventListener('submit', (e) => {
+     e.preventDefault()
+     //Consulto datos de formulario
+})
 
 socket.emit("mensaje", "Hola, este es mi primera info al servidor") // Enviar informacion a mi servidor
 
